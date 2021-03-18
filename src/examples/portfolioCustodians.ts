2591eb2ca4c8cc0fb736d7dcd9e52eeb1a68f18a
Victor Vicente <VictorVicente@users.noreply.github.com>
2021-03-18 18:26:33 -0300

    Update src/examples/portfolioCustodians.ts
    Co-authored-by: Jeremías Díaz <monitz87@gmail.com>

--- src/examples/portfolioCustodians.ts
+++ src/examples/portfolioCustodians.ts
@@ -71,7 +71,7 @@ import { getClient } from '~/common/client';
   );
   console.log(`Portfolios owned with a third party custodian: ${ownedButNotCustodiedPortfolios}`);
 
-  // Bob can get all now owned portfolios where he is the custodian
+  // Bob can get all non owned portfolios where they are the custodian
   // getCustodiedPortfolios retrieves only portfolios owned by a different Identity but custodied by this one
   const custodiedPortfolios = await bob.portfolios.getCustodiedPortfolios();
   console.log(`Custodied Portfolios owned by a third party: ${custodiedPortfolios}`);
