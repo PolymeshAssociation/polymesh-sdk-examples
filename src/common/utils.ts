1f08a72d0fcd0da87e38c8db64154122a548358a
Prashant Bajpai <prashant.bajpai19@gmail.com>
2022-07-28 16:02:11 +0530

    refactor: remove eslint skip comment

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -18,7 +18,6 @@ export function toHumanObject(obj: unknown): unknown {
   }
 
   if (obj && typeof obj === 'object') {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return Object.keys(obj).reduce(
       (r, v) => Object.assign(r, { [v]: toHumanObject(obj[v as keyof typeof obj]) }),
       {}
