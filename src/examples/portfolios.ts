9a86cb9d07cf93ff273aa9eb8724d08bdafe7b15
Toms Veidemanis <toms@veidemanis.lv>
2022-12-07 19:59:58 +0200

    feat(portfolios): move assets

--- src/examples/portfolios.ts
+++ src/examples/portfolios.ts
@@ -1,3 +1,6 @@
+import { BigNumber } from '@polymeshassociation/polymesh-sdk';
+import { Asset, PortfolioBalance } from '@polymeshassociation/polymesh-sdk/types';
+
 import { getClient } from '~/common/client';
 
 /*
@@ -9,6 +12,27 @@ import { getClient } from '~/common/client';
     - Deletes a Portfolio
 */
 (async (): Promise<void> => {
+  /**
+   * helper function for printing portfolio balance
+   **/
+  const printBalance = (
+    balances: PortfolioBalance[],
+    portfolioName: string,
+    after?: boolean
+  ): void => {
+    console.log(
+      `\n Balance in ${portfolioName} Portfolio ${after ? 'after' : 'before'} moving assets`
+    );
+
+    balances.forEach(({ asset, total, locked }) => {
+      console.log(
+        `Balance of Asset asset ${
+          asset.ticker
+        }:\n- Total: ${total.toFormat()}\n- Locked: ${locked.toFormat()}`
+      );
+    });
+  };
+
   console.log('Connecting to the node...\n\n');
   const api = await getClient(process.env.ACCOUNT_SEED);
 
@@ -16,26 +40,74 @@ import { getClient } from '~/common/client';
   const identity = (await api.getSigningIdentity())!;
   console.log(`Connected! Signing Identity ID: ${identity.did}`);
 
+  // amount of tokens that will be issuing, moving between portfolios and redeeming
+  const amount = new BigNumber(500);
+  // ticker for Asset that will be moved between portfolios
+  const ticker = 'TOKEN_1';
+
+  // setup for issuing tokens - an asset needs to be present on the chain
+  // create an asset
+  let asset: Asset;
+
+  try {
+    asset = await api.assets.getAsset({ ticker });
+  } catch (e) {
+    const assetQ = await api.assets.createAsset({
+      ticker,
+      name: 'Token One',
+      isDivisible: true,
+      assetType: 'share',
+      requireInvestorUniqueness: false,
+    });
+    asset = await assetQ.run();
+  }
+
+  // issue tokens to the asset - will be placed in the default Portfolio
+  const issueTokensQ = await asset.issuance.issue({ amount });
+  await issueTokensQ.run();
+
   const portfolioQ = await api.identities.createPortfolio({ name: 'MY_PORTFOLIO' });
   const portfolio = await portfolioQ.run();
 
   const renameQ = await portfolio.modifyName({ name: 'NEW_NAME' });
   await renameQ.run();
 
-  const [defaultPortfolio, ...numberedPortfolios] = await identity.portfolios.getPortfolios(); // First element is always the default Portfolio
+  const [defaultPortfolio, customPortfolio] = await identity.portfolios.getPortfolios(); // First element is always the default Portfolio
 
-  const balances = await defaultPortfolio.getAssetBalances({
-    assets: ['TOKEN_1', 'TOKEN_2', 'TOKEN_3'],
+  const defaultPortfolioBalance = await defaultPortfolio.getAssetBalances({
+    assets: [ticker, 'TOKEN_2', 'TOKEN_3'],
   }); // Can be called with no arguments to fetch all balances
-  balances.forEach(({ asset, total, locked }) => {
-    console.log(
-      `Balance of Asset asset ${
-        asset.ticker
-      }:\n- Total: ${total.toFormat()}\n- Locked: ${locked.toFormat()}`
-    );
-  });
+  printBalance(defaultPortfolioBalance, 'Default');
 
-  const deleteQ = await identity.portfolios.delete({ portfolio: numberedPortfolios[0] }); // Will throw an error if the Portfolio has any assets
+  const customPortfolioBalance = await customPortfolio.getAssetBalances({
+    assets: [ticker, 'TOKEN_2', 'TOKEN_3'],
+  });
+  printBalance(customPortfolioBalance, 'NEW_NAME');
+
+  const transferQ = await defaultPortfolio.moveFunds({
+    to: customPortfolio,
+    items: [{ asset: ticker, amount }],
+  });
+  await transferQ.run();
+
+  const defaultPortfolioBalanceAfter = await defaultPortfolio.getAssetBalances({
+    assets: [ticker],
+  });
+  printBalance(defaultPortfolioBalanceAfter, 'Default', true);
+
+  const customPortfolioBalanceAfter = await customPortfolio.getAssetBalances({
+    assets: [ticker, 'TOKEN_2', 'TOKEN_3'],
+  });
+  printBalance(customPortfolioBalanceAfter, 'NEW_NAME', true);
+
+  const [{ total: tokensInCustomPortfolio }] = customPortfolioBalanceAfter;
+
+  console.log(`Redeeming ${tokensInCustomPortfolio} ${ticker} from NEW_NAME Portfolio `);
+  // redeem funds from portfolio that we added them to
+  const redeemQ = await asset.redeem({ amount: tokensInCustomPortfolio, from: customPortfolio.id });
+  await redeemQ.run();
+
+  const deleteQ = await identity.portfolios.delete({ portfolio: customPortfolio }); // Will throw an error if the Portfolio has any assets
   await deleteQ.run();
 
   await api.disconnect();
